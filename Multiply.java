02f5092732e8fe1726b54f3f9936c998d9e9f7a6
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:24:14 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+class Multiply{
+
+       public static void main(String[]args){
+         int x=23;
+         int y=43;
+         int Mul=x+y;
+
+          System.out.println("Mul of the Two Number are: "+Mul);
+}
+}
