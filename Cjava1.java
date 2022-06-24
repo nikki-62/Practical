a5e4e84829298e90a6ad2c6c44bc18748bee5a5d
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:01:07 +0530

    Update Cjava1.java

--- Cjava1.java
+++ Cjava1.java
@@ -1,6 +1,6 @@
-class Cjava1{
+class Cjava1{ //Class means is your file name and both have to be same.
       public static void main(String[]args){
-        System.out.println("Name:Shilpi Ghosh");
+        System.out.println("Name:Shilpi Ghosh"); //Used to print Name
         System.out.println("Course: JAVA");
         System.out.println("Stream:ECE");
 }
