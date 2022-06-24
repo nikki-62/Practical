6bdf6f80f3fc0a9972d1e4bc2de70ba94c750830
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:29:10 +0530

    Update Getting_input.java

--- Getting_input.java
+++ Getting_input.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner;  //import the scannaer 
 class Getting_input{
 
 
        public static void main(String[]args){
           
-          System.out.println("Enter Your Name: ");
-           Scanner sc = new Scanner(System.in);
-           String Name = sc.next();
+          System.out.println("Enter Your Name: "); //used to getting input from user
+           Scanner sc = new Scanner(System.in);  //System.in is a standard input stream
+           String Name = sc.next(); //reads string
           System.out.println("Hello " + Name + " Have a good day");
 }
 }
