97000d2f30d405e1c7df27901eebf33d2c25ed82
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 10:58:58 +0530

    Update Cjava.java

--- Cjava.java
+++ Cjava.java
@@ -1,6 +1,6 @@
 class Cjava{
    public static void main (String[]args){
-    System.out.println("Welcome to coading world.");
+    System.out.println("Welcome to coading world."); //Used for print the program.
 }
 
 
