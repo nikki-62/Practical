e616c92311bf251d8b66b174923802f261bc5520
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:04:19 +0530

    Update Cjava2.java

--- Cjava2.java
+++ Cjava2.java
@@ -1,12 +1,12 @@
 class Cjava2{
   public static void main(String[]args){
-    int X=24;
+    int X=24;           //int means Integer data type
      int Y=8;
-     int Sum=X+Y;
-     int Mul=X*Y;
+     int Sum=X+Y;    //Used Sum for adding x and y
+     int Mul=X*Y;        //Used for multiplication of x and y
      int Div=X/Y;
-     System.out.println("Sum of the number is : " +Sum);
-     System.out.println("Div of the number is : " +Div);
-     System.out.println("Mul of the number is : " +Mul);
+     System.out.println("Sum of the number is : " +Sum);   //Print the sum of the two numbers.
+     System.out.println("Div of the number is : " +Div);       //used to print division of the two numbers
+     System.out.println("Mul of the number is : " +Mul);          //used to print multiplication of the two number.
 }
 }
