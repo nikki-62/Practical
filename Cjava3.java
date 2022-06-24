80b309161091f39d71857ddcb75a7c91136f72b8
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:23:29 +0530

    Add files via upload

---
+++ Cjava3.java
@@ -0,0 +1,8 @@
+class Cjava3{
+     public static void main(String[]args){
+        float x=23.4f;
+        float y=43.5f;
+        float Mul=x*y;
+        System.out.println("Mul of the number is :  " +Mul);
+}
+}
