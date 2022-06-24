e8ee04cf6be180eb17775afd4899710c9d04afb7
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:26:09 +0530

    Update Cjava3.java

--- Cjava3.java
+++ Cjava3.java
@@ -1,8 +1,8 @@
 class Cjava3{
      public static void main(String[]args){
-        float x=23.4f;
+        float x=23.4f;   //Floating point number.
         float y=43.5f;
         float Mul=x*y;
-        System.out.println("Mul of the number is :  " +Mul);
+        System.out.println("Mul of the number is :  " +Mul);  //print the multiplication of two number.
 }
 }
