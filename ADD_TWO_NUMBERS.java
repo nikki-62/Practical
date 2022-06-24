b74d1f5ed9b60c18a126533ca5bbd0c66047f25f
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:25:03 +0530

    Add files via upload

---
+++ ADD_TWO_NUMBERS.java
@@ -0,0 +1,10 @@
+class ADD_TWO_NUMBERS{
+     
+        public static void main(String[]args){
+             
+              int X=23;
+              int Y=45;
+              int Sum = X+Y;
+            System.out.println("Total Number is: "+Sum);
+}
+}
